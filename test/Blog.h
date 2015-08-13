6cec366999af0e5d571c92df47efc15f3dee6dbf
ChenGang <cganee@gmail.com>
2015-08-14 00:02:08 +0800

    .

---
+++ test/Blog.h
@@ -0,0 +1,88 @@
+#ifndef _BLOG_H_
+#define _BLOG_H_
+
+struct Blog{
+	string userId;
+	string blogId;
+	string date;
+	int forward;
+	int comment;
+	int like;
+	string text;
+
+	Blog(){}
+	Blog(int forward, int comment, int like):
+		forward(forward),comment(comment),like(like){}
+
+	Blog operator +(const Blog&b)const{
+		return Blog(forward+b.forward, comment+b.comment, like+b.like);
+
+	}
+
+	void out(){
+		cout<<forward<<' '<<comment<<' '<<like<<endl;
+	}
+
+	static int atoi(char *s){
+		int len = strlen(s);
+		int ret = 0;
+		rep(i, len) 
+			ret = ret*10 + (s[i]-'0');
+		return ret;
+	}
+
+	Blog(char *s, bool flag){
+		static char buf[BUF_SIZE];
+		int len = strlen(s);
+		int idx = 0;
+		int bufLen = 0;
+		rep(i, len)
+			if (s[i]!='\t')
+				buf[bufLen++] = s[i];
+			else{
+				buf[bufLen] = 0;
+
+				if (idx==0) userId = string(buf);
+				else if (idx==1) blogId = string(buf);
+				else if (idx==2){ 
+					date = string(buf);
+					text = s+i+1;
+					break;
+				}
+
+				++idx;
+				bufLen = 0;
+			}
+			
+	}
+
+	Blog(char *s){
+		static char buf[BUF_SIZE];
+		int len = strlen(s);
+		int idx = 0;
+		int bufLen = 0;
+		rep(i, len)
+			if (s[i]!='\t')
+				buf[bufLen++] = s[i];
+			else{
+				buf[bufLen] = 0;
+
+				if (idx==0) userId = string(buf);
+				else if (idx==1) blogId = string(buf);
+				else if (idx==2) date = string(buf);
+				else if (idx==3) forward = atoi(buf);
+				else if (idx==4) comment = atoi(buf);
+				else if (idx==5){
+					like = atoi(buf);
+					text = s+i+1;
+					break;
+				}
+
+				++idx;
+				bufLen = 0;
+			}
+			
+	}
+};
+
+#endif
---
+++ test/getFeature.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <vector>
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+
+using namespace std;
+
+#define rep(i, n) for (int i=0; i<(n); ++i)
+#define repf(i, a, b) for (int i=(a); i<=(b); ++i)
+#define repd(i, a, b) for (int i=(a); i>=(b); --i)
+#define sz(a) ((int)(a).size())
+
+#define trainData "train.txt"
+#define testData "predict.txt"
+
+#define inputData trainData
+#define BUF_SIZE 100000
+
+#include "Blog.h"
+#define N 50000
+
+char buf[BUF_SIZE];
+
+vector<Blog> userBlog[N+10];
+Blog statics[N+10];
+
+vector<Blog> train;
+vector<Blog> test;
+
+map<string, int> stringToId;
+
+void readTest(){
+	FILE *foutput = fopen("test.out", "w");
+	if (foutput==NULL){
+		fprintf(stderr, "oepn file<%s> failed\n", "test.out");
+		return;
+	}
+
+	fprintf(foutput, "cnt,userForward,userComment,userLike,text,forward,comment,like,userId,blogId,date\n");
+	rep(i, sz(test)){
+		Blog &blog = test[i];
+		int idx = stringToId[blog.userId];
+		int cnt = sz(userBlog[idx]);
+		Blog &stat = statics[idx];
+		fprintf(foutput, "%d,%.8lf,%.8lf,%.8lf,%d,%d,%d,%d,%s,%s,%s\n",
+				cnt,
+				stat.forward*1./cnt,
+				stat.comment*1./cnt,
+				stat.like*1./cnt,
+				sz(blog.text),
+				blog.forward,
+				blog.comment,
+				blog.like,
+				blog.userId.c_str(),
+				blog.blogId.c_str(),
+				blog.date.c_str()
+				);
+	}
+
+	fclose(foutput);
+}
+
+void readTrain(){
+	FILE *file = fopen(inputData, "r");
+	if (file==NULL){
+		fprintf(stderr, "open file<%s> failed\n", inputData);
+		return;
+	}
+
+	string fOutputName = "train.out";
+	FILE *foutput = fopen(fOutputName.c_str(), "w");
+	if (foutput==NULL){
+		fprintf(stderr, "open file<%s> failed\n", fOutputName.c_str());
+		return;
+	}
+
+
+	while (fgets(buf, BUF_SIZE, file)){
+		Blog blog(buf);
+		if (rand()%100<=80)
+			train.push_back(blog);
+		else
+			test.push_back(blog);
+	}
+	fclose(file);
+
+	int idx = 0;
+
+	rep(i, sz(train)){
+		Blog &blog =train[i];
+		int userIdx = stringToId[blog.userId];
+		if (userIdx == 0){
+			userIdx = ++idx;
+			stringToId[blog.userId] = userIdx;
+		}
+		userBlog[userIdx].push_back(blog);
+	}
+
+	// get statics for each users
+
+	repf(i, 1, idx){
+		rep(j, sz(userBlog[i]))
+			statics[i] = statics[i] + userBlog[i][j];
+	}
+
+
+	rep(i, sz(test)){
+		Blog &blog = test[i];
+		int userIdx = stringToId[blog.userId];
+		if (userIdx == 0){
+			userIdx = ++idx;
+			stringToId[blog.userId] = userIdx;
+		}
+		userBlog[userIdx].push_back(blog);
+	}
+
+	// output features of each train data
+
+
+	fprintf(foutput, "cnt,userForward,userComment,userLike,forward,comment,like,text\n");
+	rep(i, sz(train)){
+		Blog &blog = train[i];
+		int userIdx = stringToId[blog.userId];
+		Blog &stat = statics[userIdx];
+		int cnt = userBlog[userIdx].size();
+
+		fprintf(foutput, "%d,%.8lf,%.8lf,%8lf,%d,%d,%d,%d\n", 
+				cnt,
+				stat.forward*1./cnt,
+				stat.comment*1./cnt,
+				stat.like*1./cnt,
+				blog.forward,
+				blog.comment,
+				blog.like,
+				(int) blog.text.size());
+	}
+	fclose(foutput);
+
+
+}
+
+int main(){
+	srand(time(NULL));
+	readTrain();	
+	readTest();
+	return 0;
+}
+
